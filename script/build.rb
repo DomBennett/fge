3bff1994f1cab9c705169350bc3b9d921d052eb5
Dominic Bennett <dominic.john.bennett@gmail.com>
2015-12-11 22:11:43 +0000

    All complete but github info

--- script/build.rb
+++ script/build.rb
@@ -4,6 +4,10 @@
 #  jekyll build
 # Then, upload _site/ to gh-pages branch.
 
+# LIBS
+require "FileUtils"
+
+# FUNCTIONS
 def mkfm(fm)
   # return doc front matter
   res = "---
@@ -62,12 +66,23 @@ def get_contributions(doc, path)
   return author_string.sub(/,\s$/, "")
 end
 
+def get_last_change(doc, path)
+  # Return string of date since last edit
+  cmd = "cd #{path} && git log --pretty=format:'%ad' #{doc}"
+  log = `#{cmd}`
+  commits = log.split(/\n/)
+  last_change = commits[0]
+  last_change = last_change.split(/\s/)
+  last_change.delete_at(3)
+  last_change.delete_at(4)
+  return last_change.join('-')
+end
 
-def read_doc(infile)
+def read_doc(doc, path)
   # Return strings of front matter and doc text
-  text = File.read(infile)
+  text = File.read(File.join(path, doc))
   positions = text.enum_for(:scan, /\-\-\-/).map { Regexp.last_match.begin(0) }
-  text_fm = text[positions[0]..positions[1]]
+  text_fm = text[positions[0]..positions[1]+2]
   fm = {}
   for e in text_fm.split(/\n/)
     if /: /.match(e)
@@ -75,22 +90,50 @@ def read_doc(infile)
       fm[key] = value
     end
   end
-  return fm, text[positions[3]..-1]
+  return fm, text[positions[1]+2..-1]
 end
 
 def run(input_dir, output_dir)
   # Make docs in _docs_repo/ FGE friendly"
   # get docs
-  docs = Dir.glob(File.join(input_dir, "[^README]*.md"))
+  docs = Dir.entries(input_dir)
+  docs.delete_if { |e| !e.include?(".md") }
+  docs.delete("README.md")
   for doc in docs
     puts doc
+    fm, text = read_doc(doc, input_dir)
+    fm["lastchange"] = get_last_change(doc, input_dir)
+    fm["authors"] = get_contributions(doc, input_dir)
+    # TODO: is this if statement needed?
+    if !fm.has_key?("source")
+      fm["source"] = ""
+      fm["source_author"] = ""
+    end
+    fm["permalink"] = "/docs/#{doc.sub('.md', '')}/"
+    text = mkfm(fm) + text
+    File.open(File.join(output_dir, doc), 'w') { |file| file.write(text) }
   end
-  fm, doc = read_doc(doc)
-  puts fm
-  puts output_dir
 end
 
-#input_dir = '_docs_repo'
-#output_dir = '_docs'
-#run(input_dir, output_dir)
-puts get_contributions("unix_ssh_introduction.md", "_docs_repo")
+# SCRIPT
+if Dir.pwd[-3..-1] != 'fge'
+  puts "Halted: cwd must be fge/"
+  exit
+end
+input_dir = File.join(Dir.pwd, "_docs_repo")
+output_dir = File.join(Dir.pwd, "_docs")
+if !Dir.entries(".").include?("_docs")
+  Dir.mkdir("_docs")
+end
+data_dir = File.join(Dir.pwd, '_data')
+if !Dir.entries(".").include?("_docs_repo")
+  puts "Halted: no docs_repo/ found in cwd"
+  exit
+end
+if !Dir.entries(".").include?("_data")
+  Dir.mkdir("_data")
+end
+docs_index_src = File.join(input_dir, 'index.yml')
+docs_index_d = File.join(data_dir, 'docs.yml')
+FileUtils.cp(docs_index_src, docs_index_d)
+run(input_dir, output_dir)
